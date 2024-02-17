8ff38175ee21629d9aeb0c089f57163b7432be17
Selene <149156667+joanapq@users.noreply.github.com>
2024-02-17 19:06:02 +0000

    added comments to existing javascript lines to explain what they do

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const fs = require("fs");
-const path = require('path');
-const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown");
+const fs = require('fs'); //This imports the Node.js built-in File System module. The 'fs' module provides functions for interacting with the file system, allowing us to read from and write to files.
+const path = require('path'); //This imports another built-in Node.js module called 'path'. The 'path' module provides utilities for working with file and directory paths. It helps with tasks like resolving relative paths, joining paths, and extracting file extensions.
+const inquirer = require('inquirer'); //This imports the 'inquirer' module. Inquirer simplifies the process of getting user input through the command line by providing a set of interactive prompts.
+const generateMarkdown = require('./utils/generateMarkdown'); //This imports a custom module located at './utils/generateMarkdown.js'. The 'generateMarkdown' module contains functions or data related to generating markdown content.
 
 // array of questions for user
 const questions = [
