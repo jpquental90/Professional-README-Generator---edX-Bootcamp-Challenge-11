9e22b03a856fcdafba8cc2c671c37fd0389d42bc
Selene <149156667+joanapq@users.noreply.github.com>
2024-02-18 09:23:33 +0000

    updated generatemarkdown function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,34 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-    ## Description
-    ${data.description}
-    ## Table of contents
-    ${data.table-of-contents}
+  return `
+  
+  # ${data.title}
+
+  ## Description
+  ${data.description}
+  
+  ## Table of Contents
+  ${generateTableOfContents(data)}
+  
+  ## Installation
+  ${data.installation}
+  
+  ## Usage
+  ${data.usage}
+  
+  ## License
+  ${generateLicenseSection(data)}
+  
+  ## Contributing
+  ${data.contributing}
+  
+  ## Tests
+  ${data.tests}
+  
+  ## Questions
+  GitHub Profile: [${data.questions1}](https://github.com/${data.questions1})
+  
+  For additional questions, contact me at: ${data.questions2}
 `;
 }
 
